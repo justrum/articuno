c7d2cb3b2877e31c93fca950011f7fb9617942e8
Osman Hernandez <osman.sist@gmail.com>
2016-03-05 14:41:19 -0600

    added navbar dropdown with carbrands showin amount of cars pero brand as well. Moved global subscriptions to waiton handler on router configuration. Hide filter by brand on navbar on any route besides home route

--- articuno/client/js/navbar.js
+++ articuno/client/js/navbar.js
@@ -1,5 +1,37 @@
+Template.navbar.created = function() {
+	Session.setDefault('showBrandDropdown', false);
+	Tracker.autorun(() => {
+		if (Session.get('showBrandDropdown') === true) {
+			Tracker.afterFlush(() => {
+				$('.brand-dropdown').dropdown();
+			});
+		}
+	});
+}
+
 Template.navbar.events({
 	'click a.logout': () => {
 		Meteor.logout();
+	},
+	'showBrandDropdown nav.navbar': () => {
+		Session.set('showBrandDropdown', true);
+	},
+	'hideBrandDropdown nav.navbar': () => {
+		Session.set('showBrandDropdown', false);
+	}
+});
+
+Template.navbar.helpers({
+	brands: () => {
+		return CarBrands.find();
+	},
+	amountCarsBrand: function() {
+		return Cars.find({
+			brandId: this._id,
+			isActive: true
+		}).count();
+	},
+	showBrandDropdown: () => {
+		return Session.get('showBrandDropdown');
 	}
 });
