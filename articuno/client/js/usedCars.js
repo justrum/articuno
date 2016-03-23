ff35c3cc5ef2b6b9ef4d9d54605b4d359778b670
Osman Hernandez <osman.sist@gmail.com>
2016-03-23 13:45:26 -0600

    1) Added filter component 2) Added filtering to used/new cars section

--- articuno/client/js/usedCars.js
+++ articuno/client/js/usedCars.js
@@ -1,14 +1,26 @@
+Template.usedCars.created = function() {
+	this.cars = new ReactiveVar([]);
+	const cars = Cars.find({
+		isActive: true,
+		isNew: false
+	});
+	this.cars.set(cars);
+};
+
 Template.usedCars.helpers({
-	cars: function() {
-		return Cars.find({
-			isActive: true,
-			isNew: false
-		});
+	cars: () => {
+		return Template.instance().cars.get();
 	},
-	amountCars: function() {
-		return Cars.find({
-			isActive: true,
-			isNew: false
-		}).count();
+	amountCars: () => {
+		return Template.instance().cars.get().count();
+	}
+});
+
+Template.usedCars.events({
+	'filterCars .cars-sidebar': function(ev, template, cars) {
+		if (!cars) {
+			return;
+		}
+		template.cars.set(cars);
 	}
 });
